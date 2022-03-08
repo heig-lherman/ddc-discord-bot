44353098ce7318801841592657aca55925e139ea
Loïc Herman <loic@loicherman.ch>
2022-03-08 15:43:31 +0100

    chore: ping the boss

--- src/commands/Massimo.ts
+++ src/commands/Massimo.ts
@@ -1,16 +1,16 @@
 import { Command } from '../framework';
 
 const sentences = [
-    'Massimo tais toi 😡 !',
-    'Massimo ferme-la 😡 !',
-    'Massimo arrête de chanter 😡 !',
-    'Massimo sors d\'ici 😡 !',
-    'Massimo il est l\'heure du café ☕ !',
-    'Massimo elle est où ta sugar-mommy ❤ ?',
-    'Massimo tu peux chanter une chanson 🥺 ?',
-    'Massimo on t\'aime 💜💜',
-    'Non Massimo, le consentement c\'est pas marrant, mais c\'est important.',
-    'Non Massimo, 0! = 1',
+    '<@493112987415216148> tais toi 😡 !',
+    '<@493112987415216148> ferme-la 😡 !',
+    '<@493112987415216148> arrête de chanter 😡 !',
+    '<@493112987415216148> sors d\'ici 😡 !',
+    '<@493112987415216148> il est l\'heure du café ☕ !',
+    '<@493112987415216148> elle est où ta sugar-mommy ❤ ?',
+    '<@493112987415216148> tu peux chanter une chanson 🥺 ?',
+    '<@493112987415216148> on t\'aime 💜💜',
+    'Non <@493112987415216148>, le consentement c\'est pas marrant, mais c\'est important.',
+    'Non <@493112987415216148>, 0! = 1',
 ];
 
 export default new Command({
