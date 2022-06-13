10ac438b209ba0f8213fde30e627abf57cf9b814
Christophe - CC4 <roulin43@gmail.com>
2022-06-13 17:52:34 +0200

    feat(noot): add new noot command

---
+++ src/commands/noot.command.ts
@@ -0,0 +1,26 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import type { Message } from 'discord.js';
+
+const sentences = [
+    'ⁿᵒᵒᵗ ⁿᵒᵒᵗ',
+    'ɴᴏᴏᴛ ɴᴏᴏᴛ',
+    'noot noot',
+    "NOOT NOOT",
+    "ₙₒₒₜ ₙₒₒₜ",
+    'ᴺᴼᴼᵀ ᴺᴼᴼᵀ',
+    'NOOT NOOT'
+];
+
+@ApplyOptions<Command.Options>({
+    name: 'noot',
+    description: 'noot noot',
+    enabled: true,
+})
+export default class NootCommand extends Command {
+    public override async messageRun(message: Message) {
+        await message.channel.send({
+            content: sentences[Math.floor(Math.random() * sentences.length)],
+        });
+    }
+}
