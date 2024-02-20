8f3cf3b4054c50879a72bc6537fb9fa68a2439df
Loïc Herman <loic@loicherman.ch>
2024-02-20 20:58:05 +0000

    revert(twitch): use eventsub over http due to incompatibility with websockets

--- src/services/twitch-events.service.ts
+++ src/services/twitch-events.service.ts
@@ -2,22 +2,32 @@ import type { SapphireClient } from '@sapphire/framework';
 import { ApiClient } from '@twurple/api';
 import { AppTokenAuthProvider } from '@twurple/auth';
 import type { EventSubStreamOnlineEvent } from '@twurple/eventsub-base';
-import { EventSubWsListener } from '@twurple/eventsub-ws';
+import {
+    EventSubHttpListener,
+    ReverseProxyAdapter,
+} from '@twurple/eventsub-http';
 
 const TWITCH_USER_IDS = ['81936976', '756569499', '538667021'];
 
-const apiClient = new ApiClient({
-    authProvider: new AppTokenAuthProvider(
-        process.env.TWITCH_CLIENT_ID ?? '',
-        process.env.TWITCH_CLIENT_SECRET ?? '',
-    ),
-});
+const authProvider = new AppTokenAuthProvider(
+    process.env.TWITCH_CLIENT_ID ?? '',
+    process.env.TWITCH_CLIENT_SECRET ?? '',
+);
 
-const listener = new EventSubWsListener({ apiClient });
-listener.start();
+const apiClient = new ApiClient({ authProvider });
+const listener = new EventSubHttpListener({
+    apiClient,
+    adapter: new ReverseProxyAdapter({
+        hostName: process.env.TWITCH_EVENTSUB_DOMAIN ?? '',
+        port: Number(process.env.TWITCH_EVENTSUB_PORT ?? 8080),
+    }),
+    secret: process.env.TWITCH_SUBSCRIPTION_SECRET ?? '',
+    strictHostCheck: true,
+});
 
 export const twitchEventListener = listener;
 export const subscribeTwitchEvents = async (client: SapphireClient) => {
+    listener.start();
     TWITCH_USER_IDS.map((userId) =>
         listener.onStreamOnline(userId, (e) => {
             client.logger.info('streamOnline', e);
